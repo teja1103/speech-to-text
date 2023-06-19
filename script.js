664d66747b1901661a5936e121093a1cc5b876f1
teja1103 <115361818+teja1103@users.noreply.github.com>
2023-06-19 13:57:01 +0530

    third commit

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 runSpeechRecog = () => {
-    document.getElementById("output").innerHTML="Ruko zaraa sabar karo";
+    
     var output = document.getElementById('output');
     var action = document.getElementById('action');
     let recognization = new webkitSpeechRecognition();
-    recognization.onstart = () => {
-        action.innerHTML="I'm Waiting";
-    };
+    
     recognization.onresult = (e) =>{ 
         var transcript = e.results[0][0].transcript;
         output.innerHTML = transcript;
